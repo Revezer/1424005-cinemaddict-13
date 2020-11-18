51fc59c59f4a04313b9cae8c52e74e517cef549c
Revezer <64742445+Revezer@users.noreply.github.com>
2020-11-18 17:26:54 +0300

    отрисовка

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import {userRank} from "./view/userrank.js";
 import {navigation} from "./view/navigation.js";
 import {sort} from "./view/sort.js";
-import {films} from "./view/films.js";
-import {filmsContainer} from "./view/filmscontainer.js";
+import {films, filmSection} from "./view/films.js";
 import {film} from "./view/film.js";
+import {buttonShowMore} from "./view/button.js";
 
 const MAX_FILMS = 5;
+const MAX_FILMS_EXTRA = 2;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -19,12 +20,28 @@ render(mainElement, navigation, `beforeend`);
 render(mainElement, sort, `beforeend`);
 render(mainElement, films, `beforeend`);
 
-const filmsElement = mainElement.querySelector(`.films-list`);
+const filmsElement = mainElement.querySelector(`.films`);
 
-render(filmsElement, filmsContainer, `beforeend`);
+render(filmsElement, filmSection(``, `visually-hidden`, `All movies. Upcoming`), `beforeend`);
 
-const filmsListElement = filmsElement.querySelector(`.films-list__container`);
+const filmsListContainerElement = filmsElement.querySelector(`.films-list__container`);
 
 for (let i = 0; i < MAX_FILMS; i++) {
-  render(filmsListElement, film, `beforeend`);
+  render(filmsListContainerElement, film, `beforeend`);
+}
+
+const filmsListElement = mainElement.querySelector(`.films-list`);
+
+render(filmsListElement, buttonShowMore, `beforeend`);
+render(filmsElement, filmSection(`films-list--extra`, ``, `Top rated movies`), `beforeend`);
+render(filmsElement, filmSection(`films-list--extra`, ``, `Most commented`), `beforeend`);
+
+const filmsListContainerElements = filmsElement.querySelectorAll(`.films-list__container`);
+
+for (let i = 0; i < MAX_FILMS_EXTRA; i++) {
+  render(filmsListContainerElements[1], film, `beforeend`);
+}
+
+for (let i = 0; i < MAX_FILMS_EXTRA; i++) {
+  render(filmsListContainerElements[2], film, `beforeend`);
 }
--- src/view/films.js
+++ src/view/films.js
@@ -1,7 +1,15 @@
 export const films = () => {
   return `<section class="films">
-    <section class="films-list">
-      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-    </section>
   </section>`;
 };
+
+export const filmSection = (extra, hidden, text) => {
+  return `<section class="films-list ${extra}">
+  <h2 class="films-list__title ${hidden}">${text}</h2>
+  <div class="films-list__container">
+  </div>
+</section>`;
+};
+
+// visually-hidden
+// All movies. Upcoming
